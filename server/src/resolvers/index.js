44737f8417c142a060431ee378a1802e075ed953
faerylay <faerylay@gmail.com>
2022-02-06 16:56:31 +0900

    first commit

---
+++ server/src/resolvers/index.js
@@ -0,0 +1,5 @@
+import user from './user'
+import posts from './posts'
+import comments from './comments'
+
+export default [user, posts, comments]
---
+++ server/src/typeDefs/index.js
@@ -0,0 +1,5 @@
+import comments from './comments'
+import posts from './posts'
+import user from './user'
+import root from './root'
+export default [root, user, posts, comments]
